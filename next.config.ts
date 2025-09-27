c8cb26c5f28e08ab0f4b5e00ebcce132a867e671
matthiasmoke <matthias.rauch.98@outlook.com>
2025-09-27 21:13:48 +0200

    Adapt workflow

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/advanced-combobox",
-  assetPrefix: "/advanced-combobox",
+  basePath: process.env.PAGES_BASE_PATH || "",
+  assetPrefix: process.env.PAGES_BASE_PATH || "",
   images: {
     unoptimized: true,
   },
